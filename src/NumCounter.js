a3f820714753b82bda1981ea0ebacce8661d70f9
Tomiwa Folorunso <78617644+tommichaels@users.noreply.github.com>
2022-11-30 02:27:41 +0100

    Update NumCounter.js

--- src/NumCounter.js
+++ src/NumCounter.js
@@ -8,7 +8,7 @@ const NumCounter = () => {
 	// deploy simple storage contract and paste deployed contract address here. This value is local ganache chain
 	// let provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
   
-  let contractAddress = 'Metamask Celo Testnet wallet address';
+  let contractAddress = 'Your Metamask Celo Testnet wallet address';
 
 	const [errorMessage, setErrorMessage] = useState(null);
 	const [defaultAccount, setDefaultAccount] = useState(null);
